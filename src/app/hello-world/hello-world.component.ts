0fba665000d70b941f4e3e4d80cd5141131e7383
unknown <you@example.com>
2019-04-24 22:57:12 -0300

    teste-hello-world

---
+++ src/app/hello-world/hello-world.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-hello-world",
+  templateUrl: "./hello-world.component.html",
+  styleUrls: ["./hello-world.component.css"]
+})
+export class HelloWorldComponent {
+  titulo = "testando hello world";
+}
